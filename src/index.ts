926fe97f6e429c9d2c46ba2c7f85f4e57a43abe5
dave <daveg7lee@gmail.com>
2020-12-20 20:04:12 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-const name = "dave"
+import { prependListener } from "process";
 
-const sayHi = (name:String):String => {
-    return `You are ${name}`
+interface Human {
+    name: String,
+    gender: String,
+    age: Number
 }
 
-console.log(sayHi(name))
+const person = {
+    name:"Dave",
+    gender: "male",
+    age:20
+}
+
+const sayHi = (person:Human):String => {
+    return `You are ${person.name}, age: ${person.age}, gender: ${person.gender}`
+}
+
+console.log(sayHi(person))
 
 export {};
