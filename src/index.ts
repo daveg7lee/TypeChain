84bc76945ac065483563e292bb90ce15ce55cd25
dave <daveg7lee@gmail.com>
2020-12-20 21:01:04 +0900

    Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as CryptoJS from "crypto-js"
+import { create } from "domain";
 
 class Block {
     public index: number;
@@ -31,6 +32,16 @@ const getBlockChain = ():Block[] => blockchain
 const getLatestBlock = ():Block => blockchain[blockchain.length - 1]
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data:string):Block =>{
+    const previousBlock:Block = getLatestBlock()
+    const newIndex:number = previousBlock.index + 1
+    const newTimeStamp :number = getNewTimeStamp()
+    const newHash:string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data)
+    const newBlock:Block =  new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp)
+    return newBlock
+}
+
 
 console.log(blockchain)
 
